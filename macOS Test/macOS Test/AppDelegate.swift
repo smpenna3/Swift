cdf5076caec67384f11d48d8e4da1f4cf3ed9e0d
smpenna3 <smpenna3@gmail.com>
2016-07-14 16:47:22 -0400

    macOS Test

---
+++ macOS Test/macOS Test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS Test
+//
+//  Created by Seth Penna on 7/14/16.
+//  Copyright © 2016 Seth Penna. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ macOS Test/macOS TestTests/macOS_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  macOS_TestTests.swift
+//  macOS TestTests
+//
+//  Created by Seth Penna on 7/14/16.
+//  Copyright © 2016 Seth Penna. All rights reserved.
+//
+
+import XCTest
+@testable import macOS_Test
+
+class macOS_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
